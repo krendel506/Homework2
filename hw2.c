5517e988b306d03fed53af6d4f92285407382d99
krendel506 <krendel506@gmail.com>
2024-10-09 15:11:25 +0300

    v0.1

--- hw2.c
+++ hw2.c
@@ -1,17 +1,20 @@
 // Online C compiler to run C program online
 #include <stdio.h>
+#include<stdlib.h>
 
 int main() {
    float x, e = 0;
    float y[15];
+   int k = -1;
    printf("Enter x: ");
    scanf("%f",&x);
    do{
        printf("Enter e: ");
        scanf("%f",&e);
    }while(e<=0 || e>=1);
-    
-    for(int k =0; k<15;k++){
+
+    do{
+        k = k + 1;
          if(k == 0){
              y[k] = 1;
          }
@@ -19,12 +22,15 @@ int main() {
              y[k] = x;
          }
          else if(k== 2){
-             y[k] = 5/8;
+             y[k] = 5.0/8.0;
          }
          else{
              y[k]= y[k-2]/2 + (3*y[k-3]) / 4;
          }
+}while(k<15 || abs(y[k] - y[k-1]) >= e);
+    
+    for(int i = 0; i<= k; i++){
+        printf("%d = %f\n",i, y[i]);
     }
-    printf("%f", y[3]);
     return 0;
 }
