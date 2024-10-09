4b93f795489a840d368136e4e3b71067400be8de
krendel506 <krendel506@gmail.com>
2024-10-09 16:03:27 +0300

    v0.2 - added functions and prototypes

--- hw2.c
+++ hw2.c
@@ -1,36 +1,50 @@
-// Online C compiler to run C program online
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
+#include <math.h>  // For fabs()
+
+void f_print(float y[], int size);
+float calculating_y(float y[], int *k, float x);
 
 int main() {
-   float x, e = 0;
-   float y[15];
-   int k = -1;
-   printf("Enter x: ");
-   scanf("%f",&x);
-   do{
-       printf("Enter e: ");
-       scanf("%f",&e);
-   }while(e<=0 || e>=1);
+    float x, e = 0;
+    float y[15];
+    int k = -1;
 
-    do{
-        k = k + 1;
-         if(k == 0){
-             y[k] = 1;
-         }
-         else if(k == 1){
-             y[k] = x;
-         }
-         else if(k== 2){
-             y[k] = 5.0/8.0;
-         }
-         else{
-             y[k]= y[k-2]/2 + (3*y[k-3]) / 4;
-         }
-}while(k<15 || abs(y[k] - y[k-1]) >= e);
+    //input
+    printf("Enter x: ");
+    scanf("%f", &x);
+    do {
+        printf("Enter e: ");
+        scanf("%f", &e);
+    } while (e <= 0 || e >= 1);
+
+    //calculation
+    do {
+        k++;
+        y[k] = calculating_y(y, &k, x);
+    } while (k < 14 && fabs(y[k] - y[k-1]) >= e);
     
-    for(int i = 0; i<= k; i++){
-        printf("%d = %f\n",i, y[i]);
-    }
+    f_print(y, k);
+
     return 0;
+}
+
+// Function to calculate each y[k]
+float calculating_y(float y[], int *k, float x) {
+    if (*k == 0) {
+        return 1.0;
+    } else if (*k == 1) {
+        return x;
+    } else if (*k == 2) {
+        return 5.0 / 8.0;  // Perform floating-point division
+    } else {
+        return (y[*k - 2] / 2.0) + (3.0 * y[*k - 3] / 4.0);
+    }
+}
+
+// Function to print the array
+void f_print(float y[], int size) {
+    for (int i = 0; i <= size; i++) {
+        printf("%d. %f\n", i + 1, y[i]);
+    }
 }
